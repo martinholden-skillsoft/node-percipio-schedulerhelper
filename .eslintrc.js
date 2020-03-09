9181e9c9296e4088e438301f350cf9510028ac62
martinholden-skillsoft <12408585+martinholden-skillsoft@users.noreply.github.com>
2020-03-09 11:46:22 +0000

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,25 @@
 module.exports = {
-  "extends": [
-    "airbnb",
-    "plugin:prettier/recommended",
-    "prettier/react"
-  ],
-  "env": {
-    "browser": true,
-    "commonjs": true,
-    "es6": true,
-    "jest": true,
-    "node": true
+  extends: ['airbnb-base', 'plugin:prettier/recommended', 'prettier'],
+  env: {
+    es6: true,
+    node: true
   },
-  "rules": {
-    "jsx-a11y/href-no-hash": ["off"],
-    "react/jsx-filename-extension": ["warn", {
-      "extensions": [".js", ".jsx"]
-    }],
-    "max-len": [
-      "warn",
+  rules: {
+    'max-len': [
+      'warn',
       {
-        "code": 100,
-        "tabWidth": 2,
-        "comments": 100,
-        "ignoreComments": false,
-        "ignoreTrailingComments": true,
-        "ignoreUrls": true,
-        "ignoreStrings": true,
-        "ignoreTemplateLiterals": true,
-        "ignoreRegExpLiterals": true
+        code: 100,
+        tabWidth: 2,
+        comments: 100,
+        ignoreComments: false,
+        ignoreTrailingComments: true,
+        ignoreUrls: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+        ignoreRegExpLiterals: true
       }
-    ]
-  }
-}
+    ],
+    'no-underscore-dangle': ['error', { allowAfterThis: true }]
+  },
+  parser: 'babel-eslint'
+};
